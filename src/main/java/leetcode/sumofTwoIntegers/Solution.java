0d0b5f12e630b386294820a266b61a4d4bbbb505
Pap Lőrinc <paplorinc@gmail.com>
2017-02-15 21:09:55 +0200

    Sum of Two Integers

---
+++ src/main/java/leetcode/sumofTwoIntegers/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.sumofTwoIntegers;
+
+import java.util.*;
+import java.util.stream.*;
+import java.util.function.*;
+import leetcode.*;
+
+/**
+ * Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+ * 
+ * Example:
+ * Given a = 1 and b = 2, return 3.
+ * 
+ * 
+ * Credits:Special thanks to @fujiaozhu for adding this problem and creating all test cases.
+
+ * Source: https://leetcode.com/problems/sum-of-two-integers/
+ */
+public class Solution {
+    public int getSum(int a, int b) {
+        if (b == 0) return a;
+        else return getSum(a ^ b, (a & b) << 1) ;
+    }
+}
