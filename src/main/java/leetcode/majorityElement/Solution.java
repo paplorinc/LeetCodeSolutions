1a5309cc4e228837372a292135634e620f75a866
Pap Lőrinc <paplorinc@gmail.com>
2017-02-15 21:09:44 +0200

    Majority Element

---
+++ src/main/java/leetcode/majorityElement/Solution.java
@@ -0,0 +1,29 @@
+package leetcode.majorityElement;
+
+import java.util.*;
+import java.util.stream.*;
+import java.util.function.*;
+import leetcode.*;
+
+/**
+ * Given an array of size n, find the majority element. The majority element is the element that appears more than &lfloor; n/2 &rfloor; times.
+ * 
+ * You may assume that the array is non-empty and the majority element always exist in the array.
+ * 
+ * Credits:Special thanks to @ts for adding this problem and creating all test cases.
+
+ * Source: https://leetcode.com/problems/majority-element/
+ */
+public class Solution {
+    public int majorityElement(int[] nums) {
+        int majority = nums[0], count = 0;
+        for (int num : nums) {
+            if (num == majority) count++;
+            else if (--count < 0) {
+                majority = num;
+                count = 0;
+            }
+        }
+        return majority;
+    }
+}
