1941938c93738c9b6a6e92da70e61ceb82d4b656
Pap Lőrinc <paplorinc@gmail.com>
2017-02-22 21:09:02 +0200

    Factorial Trailing Zeroes

---
+++ src/main/java/leetcode/factorialTrailingZeroes/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.factorialTrailingZeroes;
+
+import java.util.*;
+import java.util.stream.*;
+import java.util.function.*;
+import leetcode.*;
+
+/**
+ * Given an integer n, return the number of trailing zeroes in n!.
+ * 
+ * Note: Your solution should be in logarithmic time complexity.
+ * 
+ * Credits:Special thanks to @ts for adding this problem and creating all test cases.
+
+ * Source: https://leetcode.com/problems/factorial-trailing-zeroes/
+ */
+public class Solution {
+    public int trailingZeroes(int n) {
+        int ans=0;
+        while (n>=5) {
+            n/=5;
+            ans+=n;
+        }
+        return ans;
+    }
+}
