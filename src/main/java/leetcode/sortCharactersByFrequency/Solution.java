43d3dbec43194b607b3800c89e489f5b32ffe9f4
Pap Lőrinc <paplorinc@gmail.com>
2017-03-01 21:08:28 +0200

    Sort Characters By Frequency

---
+++ src/main/java/leetcode/sortCharactersByFrequency/Solution.java
@@ -0,0 +1,63 @@
+package leetcode.sortCharactersByFrequency;
+
+import java.util.*;
+import java.util.stream.*;
+import java.util.function.*;
+import leetcode.*;
+
+/**
+ * Given a string, sort it in decreasing order based on the frequency of characters.
+ * 
+ * Example 1:
+ * 
+ * Input:
+ * "tree"
+ * 
+ * Output:
+ * "eert"
+ * 
+ * Explanation:
+ * 'e' appears twice while 'r' and 't' both appear once.
+ * So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input:
+ * "cccaaa"
+ * 
+ * Output:
+ * "cccaaa"
+ * 
+ * Explanation:
+ * Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
+ * Note that "cacaca" is incorrect, as the same characters must be together.
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * Input:
+ * "Aabb"
+ * 
+ * Output:
+ * "bbAa"
+ * 
+ * Explanation:
+ * "bbaA" is also a valid answer, but "Aabb" is incorrect.
+ * Note that 'A' and 'a' are treated as two different characters.
+ * 
+ * 
+
+ * Source: https://leetcode.com/problems/sort-characters-by-frequency/
+ */
+public class Solution {
+    public String frequencySort(String s) {
+        return s.chars().mapToObj(c -> String.valueOf((char) c))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .flatMap(e -> Collections.nCopies(e.getValue().intValue(), e.getKey()).stream())
+                .collect(Collectors.joining());
+    }
+}
