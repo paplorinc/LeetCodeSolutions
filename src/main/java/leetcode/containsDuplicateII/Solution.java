8f59647c2853cc2e88d0daad35ae3f01690ec1d5
Pap Lőrinc <paplorinc@gmail.com>
2017-03-01 21:08:45 +0200

    Contains Duplicate II

---
+++ src/main/java/leetcode/containsDuplicateII/Solution.java
@@ -0,0 +1,25 @@
+package leetcode.containsDuplicateII;
+
+import java.util.*;
+import java.util.stream.*;
+import java.util.function.*;
+import leetcode.*;
+
+/**
+ * 
+ * Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+ * 
+
+ * Source: https://leetcode.com/problems/contains-duplicate-ii/
+ */
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> indices = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            Integer lastIndex = indices.put(nums[i], i);
+            if (lastIndex != null && (i - lastIndex) <= k)
+                return true;
+        }
+        return false;
+    }
+}
