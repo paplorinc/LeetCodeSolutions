5b06a09769b77e0de80bbdc4eabf8f5109d9d25b
Pap Lőrinc <paplorinc@gmail.com>
2017-03-20 21:08:08 +0200

    Top K Frequent Elements

---
+++ src/main/java/leetcode/topKFrequentElements/Solution.java
@@ -0,0 +1,41 @@
+package leetcode.topKFrequentElements;
+
+import java.util.*;
+import java.util.stream.*;
+import java.util.function.*;
+import leetcode.*;
+
+/**
+ * 
+ * Given a non-empty array of integers, return the k most frequent elements.
+ * 
+ * For example,
+ * Given [1,1,1,2,2,3] and k = 2, return [1,2].
+ * 
+ * 
+ * Note:
+ * 
+ * You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+ * Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+ * 
+
+ * Source: https://leetcode.com/problems/top-k-frequent-elements/
+ */
+import java.util.*;
+import java.util.stream.*;
+public class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer, Long> frequencies = Arrays.stream(nums).boxed().collect(Collectors.groupingBy(i -> i, Collectors.counting()));
+        long limit = countLimit(frequencies.values(), k);
+        return frequencies.entrySet().stream()
+                          .filter(e -> e.getValue() >= limit)
+                          .limit(k)
+                          .map(Map.Entry::getKey).collect(Collectors.toList());
+    }
+    private static long countLimit(Collection<Long> values, int k) {
+        return values.stream()
+                     .sorted(Comparator.reverseOrder())
+                     .skip(k - 1)
+                     .findFirst().orElse(1L); 
+    }
+}
