8b6326db2b0d6cd01509850611e33b718acf4933
Pap Lőrinc <paplorinc@gmail.com>
2017-04-10 21:19:35 +0300

    Summary Ranges

---
+++ src/main/java/leetcode/summaryRanges/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.summaryRanges;
+
+import java.util.*;
+import java.util.stream.*;
+import java.util.function.*;
+import leetcode.*;
+
+/**
+ * Given a sorted integer array without duplicates, return the summary of its ranges.
+ * For example, given [0,1,2,4,5,7], return ["0->2","4->5","7"].
+ * Credits:Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+ *
+ * Source: https://leetcode.com/problems/summary-ranges/
+ */
+public class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> results = new ArrayList<>();
+        for (int start = 0, end = 1; end <= nums.length; end++)
+            if ((end == nums.length) || ((1 + nums[end - 1]) != nums[end])) {
+                String suffix = (1 + start != end) ? "->" + nums[end - 1] : "";
+                results.add(nums[start] + suffix);
+                start = end;
+            }
+        return results;
+    }
+}
